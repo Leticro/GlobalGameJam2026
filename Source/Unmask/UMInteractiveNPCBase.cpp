92b7a06b8529e5a4467b336a3795b9f87b66c588
Leticro <kevin.chen@shipwrightstudios.com>
2026-02-01 00:03:55 -0600

    add to main level props and first test npc

--- Source/Unmask/UMInteractiveNPCBase.cpp
+++ Source/Unmask/UMInteractiveNPCBase.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "UnmaskPlayerController.h"
 #include "UMInteractiveNPCBase.h"
+#include "UnmaskPlayerController.h"
 
 // Sets default values
 AUMInteractiveNPCBase::AUMInteractiveNPCBase()
